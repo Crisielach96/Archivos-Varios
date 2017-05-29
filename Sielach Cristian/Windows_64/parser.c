a9dbfdcd363833c2060162b38039f0af871ff455
Cristian Sielach <crisielach@yahoo.com.ar>
2017-05-29 12:03:54 -0300

    asd

---
+++ Sielach Cristian/Windows_64/parser.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+
+int parserEmployee(FILE* pFile , ArrayList* pArrayListEmployee)
+{
+    FILE * parch;
+    if((parch=fopen("data.csv","r+"))==NULL)
+    {
+        printf("El archivo no existe.");
+        exit(1);
+    }
+    else
+    {
+        printf("El archivo se abrio correctamente\n");
+    }
+
+    return 0;
+}
